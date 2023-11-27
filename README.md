e0d86358c6eb0b7c4cac02beae04f425c6b2783b
tmychow <tmychow@gmail.com>
2023-11-27 15:05:36 -0800

    lognormal

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # power-laws
-A Python notebook of "the secretary problem meets power laws"
+A Python notebook of "the secretary problem meets heavy-tailed distributions"
---
+++ analysis.py
@@ -0,0 +1,19 @@
+# https://www.benkuhn.net/outliers/
+
+import numpy as np
+
+def simulate_log_normal(mu, sigma, n, m):
+    """Simulate a log-normal distribution."""
+    initial_samples = np.random.lognormal(mean=mu, sigma=sigma, size=n)
+    threshold = np.max(initial_samples) * 5
+    new_samples = np.random.lognormal(mean=mu, sigma=sigma, size=m)
+    return int(any(new_samples > threshold))
+
+def monte_carlo(mu, sigma, n, m, num_trials):
+    """Run a simulation for a set of parameters."""
+    results = []
+    for i in range(num_trials):
+        results.append(simulate_log_normal(mu, sigma, n, m))
+    return np.mean(results)
+
+print(monte_carlo(0, 1, 500, 1000, 10000))
