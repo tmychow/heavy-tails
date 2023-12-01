6c3eb52f6cd5cd672524bcef78c677720dd8d1a6
tmychow <tmychow@gmail.com>
2023-12-01 12:59:03 -0800

    sixty

--- analysis.py
+++ analysis.py
@@ -5,7 +5,7 @@ import numpy as np
 def simulate_log_normal(mu, sigma, n, m):
     """Simulate a log-normal distribution."""
     initial_samples = np.random.lognormal(mean=mu, sigma=sigma, size=n)
-    threshold = np.max(initial_samples) * 50
+    threshold = np.max(initial_samples) * 10
     new_samples = np.random.lognormal(mean=mu, sigma=sigma, size=m)
     return int(any(new_samples > threshold))
 
@@ -16,4 +16,4 @@ def monte_carlo(mu, sigma, n, m, num_trials):
         results.append(simulate_log_normal(mu, sigma, n, m))
     return np.mean(results)
 
-print(monte_carlo(0, 1, 500, 500, 1000))
+print(monte_carlo(0, 1, 400, 600, 10000))
